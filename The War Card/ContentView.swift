405831dc58733524fb8c2fd535d553bd96e41ccd
Asbiq Al Alawi <asbiqalalawi@gmail.com>
2023-08-03 21:09:38 +0700

    first commit

---
+++ The War Card/ContentView.swift
@@ -0,0 +1,84 @@
+//
+//  ContentView.swift
+//  The War Card
+//
+//  Created by SDS.Mac.012 on 03/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var playerCard = "card7"
+    @State var cpuCard = "card14"
+    
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
+    var body: some View {
+        ZStack {
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                Button {
+                    deal()
+                } label: {
+                    Image("button")
+                }
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                Spacer()
+            }
+        }
+    }
+    
+    func deal() {
+        let playerCardValue = Int.random(in: 2...14)
+        let cpuCardValue = Int.random(in: 2...14)
+        
+        playerCard = "card" + String(playerCardValue)
+        cpuCard = "card" + String(cpuCardValue)
+        
+        if (playerCardValue > cpuCardValue) {
+            playerScore += 1
+        } else if (cpuCardValue > playerCardValue) {
+            cpuScore += 1
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ The War Card/The_War_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_War_CardApp.swift
+//  The War Card
+//
+//  Created by SDS.Mac.012 on 03/08/23.
+//
+
+import SwiftUI
+
+@main
+struct The_War_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
